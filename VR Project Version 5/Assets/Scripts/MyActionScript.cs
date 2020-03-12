8371dc2a19d1dcee96e66b356d0542324b51ccd6
Luke Roznovsky <lrozbck@gmail.com>
2020-03-12 08:58:39 -0500

    Version 5
    This is the penultimate version. We are only missing minor things, with
    the most obvious being Saturn’s rings. The final version will be up
    soon!

---
+++ VR Project Version 5/Assets/Scripts/MyActionScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class MyActionScript : MonoBehaviour
+{
+    //A reference to the action
+    public SteamVR_Action_Boolean SphereOnOff;
+
+    //A reference to the hadn
+    public SteamVR_Input_Sources handType;
+
+    //reference to the sphere
+    public GameObject Sphere;
+
+    void Start()
+    {
+        SphereOnOff.AddOnStateDownListener(TriggerDown, handType);
+        SphereOnOff.AddOnStateUpListener(TriggerUp, handType);
+    }
+
+    public void TriggerUp(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        print("Trigger is up");
+        Sphere.GetComponent<MeshRenderer>().enabled = false;
+    }
+
+    public void TriggerDown(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        print("Trigger is down");
+        Sphere.GetComponent<MeshRenderer>().enabled = true;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ VR Project Version 5/Assets/Scripts/OrbitMotion.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class OrbitMotion : MonoBehaviour
+{
+    public Transform orbitingObject;
+    public Ellipse orbitPath;
+
+    [Range(0f, 1f)]
+    public float orbitProgress = 0f;
+    public float orbitPeriod;
+    public bool orbitActive = true;
+
+
+    //public SteamVR_Action_Boolean grabPinch; //Grab Pinch is the trigger, select from inspecter
+    //public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
+
+    void Start()
+    {
+        if (orbitingObject == null)
+        {
+            orbitActive = false;
+            return;
+        }
+        SetOrbitingObjectPosition();
+        //StartCoroutine(AnimateOrbit());
+
+        //Create listeners
+        SteamVR_Actions.default_GrabPinch.AddOnStateDownListener(TriggerPressed, SteamVR_Input_Sources.Any);
+        SteamVR_Actions.default_GrabPinch.AddOnStateUpListener(TriggerReleased, SteamVR_Input_Sources.Any);
+
+
+
+    }
+
+    private void Update()
+    {
+        float orbitSpeed = 1f / orbitPeriod;
+        orbitProgress += Time.deltaTime * orbitSpeed;
+        orbitProgress %= 1f;
+        SetOrbitingObjectPosition();
+    }
+
+    //Listener methods for detecting if trigger is up or down
+    private void TriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        print("Trigger pressed!");
+        orbitPeriod = orbitPeriod / 3;
+    }
+
+    private void TriggerReleased(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    {
+        print("Trigger released!");
+        orbitPeriod = orbitPeriod * 3;
+    }
+
+    void SetOrbitingObjectPosition()
+    {
+        Vector3 orbitPos = orbitPath.Evaluate(orbitProgress);
+        orbitingObject.localPosition = new Vector3(orbitPos.x, 0, orbitPos.z);
+    }
+    //IEnumerator AnimateOrbit()
+    //{
+    //    if( orbitPeriod < 0.1f)
+    //    {
+    //        orbitPeriod = 0.1f;
+    //    }
+    //    float orbitSpeed = 1f / orbitPeriod;
+    //    while (orbitActive)
+    //    {
+    //        {
+    //            orbitProgress += Time.deltaTime * orbitSpeed;
+    //            orbitProgress %= 1f;
+    //            SetOrbitingObjectPosition();
+    //            yield return null;
+    //        }
+    //        return null;
+    //}
+
+}
